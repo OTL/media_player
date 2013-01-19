ade93126e126accd3d6d62d150014a2fa2eb5285
Takashi Ogura <t.ogura@gmail.com>
2013-01-19 12:26:04 +0900

    add volume_controller.py

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ movie_player
 Linux ROS movie_player node.
 This controls movie by totem. You needs totem.
 
+movie_player.py
+================
+
 Published Topics
 ----------
 - None
@@ -16,11 +19,29 @@ Subscribed Topics
 
 movie_player/PlayList message
 -----------------
-string[] paths:   full path of movie to be played
+- string[] paths:   full path of movie to be played
 
 movie_player/Control message
 -----------------
-byte type: type of control (Play:0, Pause:1, Quit:2, Next:3, Previsou:4)
+- byte type: type of control (Play:0, Pause:1, Quit:2, Next:3, Previsou:4)
+
+volume_controller.py
+================
+controls linux system volume using amixer.
+
+Subscribed Topics
+----------
+- /audio_volume (movie_player/Volume)   set system audio volume by volume percentage
+
+Parameters
+-----------
+- ~max_value (default: 255) : max of value to set
+- ~card_id (default: 0) : id of sound card
+- ~control (defalut: PCM) : this must be Master if you want to control all volumes.
+
+movie_player/Volume message
+----------
+- byte percentage : percentage of system volume
 
 
 License
---
+++ nodes/volume_controller.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+
+import rospy
+import roslib
+roslib.load_manifest("movie_player")
+
+import subprocess
+
+from movie_player.msg import Volume
+
+def set_volume_by_amixer(volume, card_id=0, control='Master'):
+    # example command line: amixer -c1 sset PCM 500
+    args = ["amixer", "-c%d"%card_id, "sset", control, str(volume)]
+    result = subprocess.call(args)
+    if result == 0:
+        return True
+    else:
+        return False
+    
+
+class VolumeController:
+    def __init__(self):
+        self.max_value = rospy.get_param('~max_value', 255)
+        self.card_id = rospy.get_param('~card_id', 0)
+        self.control = rospy.get_param('~control', 'PCM')
+        self.sub = rospy.Subscriber("/audio_volume",
+                                    Volume,
+                                    self.change_volume_by_msg)
+
+    def percentage_to_value(self, percent):
+        value = percent * 0.01 * self.max_value
+        if value > self.max_value:
+            value = self.max_value
+            rospy.logwarn("volume percent should be less than 100 (you set %d)"% percent)
+        elif value < 0:
+            value = 0
+            rospy.logwarn("volume percent should be less than 100 (you set %d)"%percent)
+        return value
+
+    def change_volume_by_msg(self, msg):
+        value = self.percentage_to_value(msg.percentage)
+        if not set_volume_by_amixer(value, card_id=self.card_id, control=self.control):
+            rospy.logerr('set volume by amixer failed')
+
+if __name__ == '__main__':
+    rospy.init_node('volume_controller')
+    node = VolumeController()
+    rospy.spin()
